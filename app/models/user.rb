7b926e4add1cc5a03cc52efaacc28090dcae432f
Peter <peterbybel@gmail.com>
2019-09-26 10:44:34 -0500

    ui views for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :user_funds
   has_many :funds, through: :user_funds
 
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+
+
   def fund_already_added?(ticker_symbol)
     fund = Fund.find_by_ticker(ticker_symbol)
     return false unless fund
