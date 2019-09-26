28afd1468b3bf767dfc4e7191e8a21ffa30114ba
Peter <peterbybel@gmail.com>
2019-09-26 12:43:13 -0500

    completed app

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   resources :user_funds, only: [:create, :destroy]
 
   get 'my_friends', to: 'users#my_friends'
+  resources :users, only: [:show]
+  resources :friendships
+
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
